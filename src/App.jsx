d5f977a427e550ecf5a3833b251aeb125a891b94
Tulza <40633228+tulza@users.noreply.github.com>
2023-11-11 02:13:45 +1300

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 function App() {
   return (
     <>
-      <h1 className="text-8xl underline text-red">Template</h1>
+      Bill Select Tip % 5% 10% 15% 25% 50% Custom Number of People Tip Amount /
+      person Total / person Reset
+      <div className="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Your Name Here</a>.
+      </div>
     </>
   );
 }
