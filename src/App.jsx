b2f01e6e1d9784fd47b235a775d7f6cbd2dec3d5
Tulza <40633228+tulza@users.noreply.github.com>
2023-11-17 07:19:06 +1300

    Init new tip app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from "react";
+import React from "react";
 import "./style/styles.css";
+import Attribution from "./Components/Attribution";
 
-function App() {
+const App = () => {
   return (
     <>
       <div className="flex justify-center align-middle">
         <h1> thisa template</h1>
       </div>
+
+      {/* ignore - for frontend mentor */}
+      <Attribution />
     </>
   );
-}
+};
 
 export default App;
---
+++ src/Components/Attribution.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Attribution = () => {
+  return (
+    <div className="attribution">
+      Challenge by{" "}
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+        Frontend Mentor
+      </a>
+      . Coded by <a href="#">Tulza</a>.
+    </div>
+  );
+};
+
+export default Attribution;
