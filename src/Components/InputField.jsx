a3ef0d746ee85a77306a0dad3bff2bf5ed54916d
Tulza <40633228+tulza@users.noreply.github.com>
2023-11-18 05:33:15 +1300

    minor fix

--- src/Components/InputField.jsx
+++ src/Components/InputField.jsx
@@ -2,9 +2,22 @@ import React from "react";
 import clsx from "clsx";
 
 const InputField = ({ value, placeholder, imageUrl, onChange, error }) => {
+  if (imageUrl) {
+    return (
+      <div className="flex">
+        <img className="input-logo" src={imageUrl} alt="input field image" />
+        <input
+          value={value}
+          type="number"
+          className={clsx("input-field", error ? "input-field-error" : "")}
+          placeholder={placeholder}
+          onChange={onChange}
+        />
+      </div>
+    );
+  }
   return (
     <div className="flex">
-      <img className="input-logo" src={imageUrl} />
       <input
         value={value}
         type="number"
